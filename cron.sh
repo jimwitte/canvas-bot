9f44c388fdedde9abec92f358002c4ef52e77e7d
Jim Witte <jwitte@illinois.edu>
2022-09-19 14:43:32 -0500

    added absent report for LAS 101

--- cron.sh
+++ cron.sh
@@ -18,11 +18,12 @@ python3 update_section_groups.py
 # LAS 101 FALL 2022
 export CANVAS_COURSE_ID=22598
 export CANVAS_GROUPSET_NAME='Section Groups'
-    # publish grade report to LAS399
+    # publish grade report and absent report to LAS399
 export CANVAS_PUBLISH_COURSE_ID=22602
 python3 user_set_section_only.py
 python3 update_section_groups.py
 python3 intern_grade_report.py
+python3 absent_report.py
 
 # LAS 399 FALL 2022
 export CANVAS_COURSE_ID=22602
