b0544f153c6031032cc9e1769864e5982e56f8d6
Jim Witte <jwitte@illinois.edu>
2024-03-15 13:23:55 -0500

    fix code indent

--- update_section_groups.py
+++ update_section_groups.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     API_URL = os.environ.get('API_URL', None)
     API_KEY = os.environ.get('API_KEY', None)
 
-    CANVAS_COURSE_ID = os.environ.get('CANVAS_COURSE_ID', None)
-    CANVAS_GROUPSET_NAME = os.environ.get('CANVAS_GROUPSET_NAME', None)
+    CANVAS_COURSE_ID = os.environ.get("CANVAS_COURSE_ID", None)
+    CANVAS_GROUPSET_NAME = os.environ.get('CANVAS_GROUPSET_NAME', 'Section Groups')
 
     if not all([API_URL, API_KEY, CANVAS_COURSE_ID, CANVAS_GROUPSET_NAME]):
         print("Required env variable(s) not found.")
@@ -35,54 +35,56 @@ if __name__ == "__main__":
         ],
     )
 
-groupset_id = None
+    logging.info(f"starting update section groups...")
 
-group_categories = course.get_group_categories()
-for groupset in group_categories:
-    if groupset.name == CANVAS_GROUPSET_NAME:
-        groupset_id = groupset.id
-        break
+    groupset_id = None
 
-if groupset_id is None:
-    # Assuming course.create_groupset(name) creates a new groupset and returns its ID
-    new_groupset = course.create_group_category(CANVAS_GROUPSET_NAME)
-    groupset_id = new_groupset.id
-    if groupset_id:
-        logging.info(f"Created new groupset with name: {CANVAS_GROUPSET_NAME}.")
-    else:
-        logging.error(f"Failed to create groupset with name: {CANVAS_GROUPSET_NAME}.")
-        exit(1)
+    group_categories = course.get_group_categories()
+    for groupset in group_categories:
+        if groupset.name == CANVAS_GROUPSET_NAME:
+            groupset_id = groupset.id
+            break
 
-    groupset = canvas.get_group_category(groupset_id)
+    if groupset_id is None:
+        # Assuming course.create_groupset(name) creates a new groupset and returns its ID
+        new_groupset = course.create_group_category(CANVAS_GROUPSET_NAME)
+        groupset_id = new_groupset.id
+        if groupset_id:
+            logging.info(f"Created new groupset with name: {CANVAS_GROUPSET_NAME}.")
+        else:
+            logging.error(f"Failed to create groupset with name: {CANVAS_GROUPSET_NAME}.")
+            exit(1)
 
-    logging.warning(f"Checking/changing section groupset: {groupset} course: {course.course_code}")
+        groupset = canvas.get_group_category(groupset_id)
 
-    groups = groupset.get_groups()
+        logging.warning(f"Checking/changing section groupset: {groupset} course: {course.course_code}")
 
-    if not groups:
-        # create groups if needed
-        sections = course.get_sections()
-        for section in sections:
-            sectionid = section.id
-            group_name = section.name
-            group = groupset.create_group(name=group_name, description=section.id)
+        groups = groupset.get_groups()
 
-    groups = groupset.get_groups()
+        if not groups:
+            # create groups if needed
+            sections = course.get_sections()
+            for section in sections:
+                sectionid = section.id
+                group_name = section.name
+                group = groupset.create_group(name=group_name, description=section.id)
 
-    for group in groups:
-        # update group membership
-        memberships = group.get_memberships()
-        time.sleep(.25)
-        group_members = set([int(member.user_id) for member in memberships])
-        group_description = group.description
-        if group_description != None:
-            sectionid = int(group.description)  #sectionid stored in group description field
-            section = course.get_section(sectionid)
-            enrollments = section.get_enrollments()
-            section_members = set([int(enrollment.user_id) for enrollment in enrollments
-                                if enrollment.type == 'StudentEnrollment' and enrollment.enrollment_state == 'active'])
-            for user_id in (section_members - group_members):
-                user = course.get_user(user_id)
-                logging.warning(f'added {user} to group: {group.name}')
-                group.create_membership(user_id)
-                time.sleep(.25)
+        groups = groupset.get_groups()
+
+        for group in groups:
+            # update group membership
+            memberships = group.get_memberships()
+            time.sleep(.25)
+            group_members = set([int(member.user_id) for member in memberships])
+            group_description = group.description
+            if group_description != None:
+                sectionid = int(group.description)  #sectionid stored in group description field
+                section = course.get_section(sectionid)
+                enrollments = section.get_enrollments()
+                section_members = set([int(enrollment.user_id) for enrollment in enrollments
+                                    if enrollment.type == 'StudentEnrollment' and enrollment.enrollment_state == 'active'])
+                for user_id in (section_members - group_members):
+                    user = course.get_user(user_id)
+                    logging.warning(f'added {user} to group: {group.name}')
+                    group.create_membership(user_id)
+                    time.sleep(.25)
